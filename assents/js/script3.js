fb53f88d8e8f3fc575328d6a6e17fed353562d8e
Javier Contreras <javiercontrerasgallardo@gmail.com>
2025-06-03 11:22:37 -0400

    commit de validacion
    intento de validar

--- assents/js/script3.js
+++ assents/js/script3.js
@@ -1,6 +1,18 @@
+//telefono debe tener exactamente 8 digitos, no es obligatoria
+//contrasenia debe tener mas de 5 caracteres, es obligatoria
+
 let datos = []
 
-let validar = 
-    (
-        
-    )
+function validar()
+    {
+        let eTelefono = document.getElementById("Teléfono")
+        let vTelefono = document.getElementById(eTelefono)
+
+    if(vTelefono.Lenght>=8)
+        {
+
+            vTelefono.style.backgroundColor="green"
+            vTelefono.style.color="white"
+            console.log(vTelefono)
+        }
+    }
