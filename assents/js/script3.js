0594536daec12fa1fcd46ae383d4bd033bce3ff0
Javier Contreras <javiercontrerasgallardo@gmail.com>
2025-06-03 17:06:41 -0400

    video

--- assents/js/script3.js
+++ assents/js/script3.js
@@ -1,5 +1,3 @@
-//telefono debe tener exactamente 8 digitos, no es obligatoria
-//contrasenia debe tener mas de 5 caracteres, es obligatoria
 let datos = []
 function validar(){
         let eTelefono = document.getElementById("telefono");
